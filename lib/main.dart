1245a38c08f00d53b58ccc6126ade47ab367308e
hrshmistry <harsh241299mistry@gmail.com>
2020-08-01 14:25:52 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+
+main() {
+  runApp(MyMusic());
+}
+
+class MyMusic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myApp();
+  }
+}
